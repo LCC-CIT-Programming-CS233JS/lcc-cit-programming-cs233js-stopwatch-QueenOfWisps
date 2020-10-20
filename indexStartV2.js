04af10d11f9fd6c4ef8bc3402d04c56a71203ab2
QueenOfWisps <brittainr@my.lanecc.edu>
2020-10-20 15:19:54 -0700

    done with class conversion

--- indexStartV2.js
+++ indexStartV2.js
@@ -1,6 +1,6 @@
 // Create a class called StopWatch.
 {
-    /*
+        /*
         Add a constructor.  In the body of the constructor
         -   Create instance variables for these 3 variables as well
             as all of the elements on the page that you're going to
@@ -12,17 +12,96 @@
             event fires
             -- this.startButton.onclick = this.startTimer.bind(this);
     */
+        class StopWatch
+    {
+            constructor()
+            {
+                this.isRunning=false; 
+                this.timer= null; 
+                this.timerTime= 0;
+                this.startBtn = document.getElementById("start");
+                this.stopBtn =  document.getElementById("stop"); 
+                this.resetBtn = document.getElementById("reset");
 
-    /*
-        Convert each function to a method.  
-        -   Move it inside the class.
-        -   Remove the keyword function
-        -   Add this. in front of every variable and method
-    */
+                this.startBtn.onclick = this.startTimer.bind(this);
+                this.stopBtn.onclick= this.stopTimer.bind(this);
+                this.resetBtn.onclick = this.resetTimer.bind(this); 
+
+            
+
+            }
+            /*
+            Convert each function to a method.  
+            -   Move it inside the class.
+            -   Remove the keyword function
+            -   Add this. in front of every variable and method
+        */
+            
+        startTimer() {
+        
+        if(this.isRunning==false){
+            this.isRunning=true;
+            this.timer=setInterval(this.incrementTimer.bind(this),1000);
+            document.getElementById("prog").style="width:33%"
+            
+            }
+
+        }
+        incrementTimer() {
+
+            
+            this.timerTime++
+            let minutes = Math.floor(this.timerTime/60);
+            let seconds = Math.floor(this.timerTime%60);
+        
+        
+            document.getElementById("minutes").innerHTML=this.pad(minutes);
+            document.getElementById("seconds").innerHTML=this.pad(seconds);
+        }
+
+        pad(number) {
+            
+            if(number<10)
+            return "0" + number;
+            else
+            return number; 
+            
+        }
+
+        stopTimer() {
+            // if the timer is running, stop it
+            if(this.isRunning==true){
+                this.isRunning=false; 
+                clearInterval(this.timer);
+            }
+            document.getElementById("prog").style="width:66%"
+            
+        
+        }
+
+        resetTimer() {
+        
+        this.stopTimer();
+        this.timerTime=0;
+        document.getElementById("minutes").innerHTML=this.pad(0);
+        document.getElementById("seconds").innerHTML=this.pad(0);
+        document.getElementById("prog").style="width:100%"
+        
+        }
+
+
+
+
+
+
+    
+    }
+
+    // create a variable called stopWatch
+    let stopWatch;
+
+
+    // Add an event handler to the load event of the window. 
+    window.onload=() => { stopWatch = new StopWatch();}
+    // Use an anonymous function or an arrow function;
 }
-
-// create a variable called stopWatch
-
-// Add an event handler to the load event of the window. 
-// Use an anonymous function or an arrow function to
-// set the stopWatch variable to an instance of StopWatch
